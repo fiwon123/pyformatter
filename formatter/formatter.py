c1fc29c9a9ce108c771dd7487e354f311f001363
DESKTOP-TJESOBL\Felipe <felipeinoue@yahoo.com.br>
2025-04-18 23:22:29 -0300

    implement auto-detection wrong syntax in json and yaml files

--- formatter/formatter.py
+++ formatter/formatter.py
@@ -1,10 +1,14 @@
-from formatter.utils import get_dir_path, join_paths, create_dir, get_file_name
+from formatter.utils import get_dir_path, join_paths, create_dir, get_file_name, error
 import json
 import yaml
 
 def format_json(filepath: str):
-    with open(filepath, "r", encoding="utf-8") as file:
-        content = json.loads(file.read())
+    try:
+        with open(filepath, "r", encoding="utf-8") as file:
+            content = json.load(file)
+    except json.JSONDecodeError as e:
+        error(f"[ERROR] Invalid JSON: {e}")
+        
 
     default_comma = ", "
     default_colon = " : "
@@ -15,16 +19,20 @@ def format_json(filepath: str):
     
     output = join_paths(output, get_file_name(filepath) + "_formatted.json")
 
-    with open(output, "w", encoding="utf-8") as file:
-        file.write(json.dumps(content, indent=4, separators=(default_comma, default_colon)))
+    try:
+        with open(output, "w", encoding="utf-8") as file:
+            file.write(json.dumps(content, indent=4, separators=(default_comma, default_colon)))
+    except TypeError as e:
+        error(f"[ERROR] Unable to serialize data to JSON: {e}")
     
     return True
 
 def format_yaml(filepath: str):
-    with open(filepath, "r", encoding="UTF-8") as file:
-        content = yaml.safe_load(file.read())
-
-    print(content)
+    try:
+        with open(filepath, "r", encoding="UTF-8") as file:
+            content = yaml.safe_load(file)
+    except yaml.YAMLError as e:
+        error(f"[ERROR] Invalid YAML: {e}") 
 
     output = get_dir_path(filepath)
     output = join_paths(output, "output")
@@ -32,8 +40,12 @@ def format_yaml(filepath: str):
 
     output = join_paths(output, get_file_name(filepath) + "_formatted.yaml")
 
-    with open(output, "w", encoding="utf-8") as file:
-        file.write(yaml.dump(content))
+    try:
+        with open(output, "w", encoding="utf-8") as file:
+            file.write(yaml.dump(content))
+    except TypeError as e:
+        error(f"[ERROR] Unable to serialize data to YAML: {e}")
+
 
     
     return True
